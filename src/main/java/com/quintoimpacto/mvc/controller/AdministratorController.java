4836c9867d3d56dac454032dc42a38816444aa95
FedeCasper <federicorouyere@gmail.com>
2023-11-16 13:01:22 -0300

    Creating administrator controller

---
+++ src/main/java/com/quintoimpacto/mvc/controller/AdministratorController.java
@@ -0,0 +1,13 @@
+package com.quintoimpacto.mvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/administrators")
+public class AdministratorController {
+
+    @Autowired
+
+}
