8eb15295871a64a1daa91a4278627b6a65f8c99a
FedeCasper <federicorouyere@gmail.com>
2023-11-17 18:54:38 -0300

    Finished read course functionality

--- src/main/java/com/quintoimpacto/mvc/controller/CourseController.java
+++ src/main/java/com/quintoimpacto/mvc/controller/CourseController.java
@@ -4,10 +4,9 @@ import com.quintoimpacto.mvc.model.Course;
 import com.quintoimpacto.mvc.service.course.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
@@ -15,6 +14,12 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courseList =  courseService.getAllCourses();
+        return courseList != null ? ResponseEntity.ok(courseList) : ResponseEntity.notFound().build();
+    }
 
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Course> activateCourse (@PathVariable("id") Long id){
