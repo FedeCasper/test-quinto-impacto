cd33c762154c22feb574b5010ab8fa747cf20e6d
FedeCasper <federicorouyere@gmail.com>
2023-11-18 16:09:34 -0300

    Created getCurrentUser service

--- src/main/java/com/quintoimpacto/mvc/service/user/UserService.java
+++ src/main/java/com/quintoimpacto/mvc/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.quintoimpacto.mvc.service.user;
 
 import com.quintoimpacto.mvc.model.User;
+import org.springframework.security.core.Authentication;
 
 public interface UserService {
 
     public void saveUser(User user);
+
+    public User getCurrentUser(Authentication authentication);
 }
--- src/main/java/com/quintoimpacto/mvc/service/user/UserServiceImpl.java
+++ src/main/java/com/quintoimpacto/mvc/service/user/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.quintoimpacto.mvc.service.user;
 import com.quintoimpacto.mvc.model.User;
 import com.quintoimpacto.mvc.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +14,9 @@ public class UserServiceImpl {
 
     public void saveUser(User user) {
         userRepository.save(user);
+    }
+
+    public User getCurrentUser(Authentication authentication) {
+        return userRepository.findByEmail(authentication.getName());
     }
 }
